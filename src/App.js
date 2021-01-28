a03344d2e3bea50fb95f9bcdfabae7bb1ddd2d0f
Kuddleman <edl1155@gmail.com>
2021-01-28 13:51:04 -0800

    add useState hook

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
 import React from 'react' 
+import Accordion from './components/Accordion'
 
-export default () => {
-    return <h1>Hellow from Widgets</h1>
+const items = 
+    [
+        {
+            title: 'What is React?',
+            content: 'React is very cool'
+        },
+        {
+            title: 'Why use React?',
+            content: 'React is a fav JS library'
+        },
+        {
+            title: 'How do you use React?',
+            content: 'You use React by creating components'
+        }
+    ]
+
+const App = () => {
+    return (     
+        <div>
+            <Accordion items={items}/>
+        </div>
+        
+    )
 }
+
+export default App
