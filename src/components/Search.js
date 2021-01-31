0eef1f0c4c7e78330692ee5ef950bd5e9091c97f
Kuddleman <edl1155@gmail.com>
2021-01-31 10:17:05 -0800

    add useEffect hook and axios

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,23 @@
-import React from 'react' 
+import React,  { useState, useEffect } from 'react' 
 
 const Search = () => {
+    const [ term, setTerm ] = useState('')
+
+    console.log('I run with EVERY render')
+
+    useEffect(()=>{
+        console.log('I RUN After every rerender')
+    }, [term])
+
     return (
         <div>
             <div className="ui form">
                 <div className="field">
-                    <label htmlFor="">Enter Search Term</label>
-                    <input type="text" className="input"/>
+                    <label>Enter Search Term</label>
+                    <input  className="input"
+                            value={ term }
+                            onChange={e => setTerm(e.target.value)}
+                    />
                 </div>
             </div>
         </div>
