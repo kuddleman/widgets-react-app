c729444fa2573f0dfb66389672f46ddbbe4fce16
Kuddleman <edl1155@gmail.com>
2021-01-31 10:34:23 -0800

    set up axios command

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,25 @@
 import React,  { useState, useEffect } from 'react' 
+import axios from 'axios' 
 
 const Search = () => {
     const [ term, setTerm ] = useState('')
 
-    console.log('I run with EVERY render')
+   
 
     useEffect(()=>{
-        console.log('I RUN After every rerender')
+       const search = async () => {
+           await axios.get('https://en.wikipedia.org/w/api.php', {
+               params: {
+                   action: 'query',
+                   list: 'search',
+                   origin: '*',
+                   format: 'json',
+                   srsearch: term
+               }
+           }) 
+       }
+
+       search()
     }, [term])
 
     return (
